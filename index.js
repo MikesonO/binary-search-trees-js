723375d8ccf21d4a97d98f7523031c8daee967f2
mikeson <mikeson.jnr@gmail.com>
2023-01-05 21:49:40 +0000

    Add prettyPrint function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import Tree from "./tree.js";
+import Tree from './tree.js';
+import drawTree from './prettyPrint.js'
 
 let arr = [4,1,3,6];
 const tree = new Tree(arr);
@@ -6,3 +7,5 @@ const tree = new Tree(arr);
 // Test
 
 console.log(tree);
+console.log(drawTree(tree.root));
+
---
+++ prettyPrint.js
@@ -0,0 +1,11 @@
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
+
+export default prettyPrint
