a41cdcebb22d92cf480453688172059e57c1dde7
mikeson <mikeson.jnr@gmail.com>
2023-01-05 21:34:57 +0000

    Add buildTree function

---
+++ index.js
@@ -0,0 +1,8 @@
+import Tree from "./tree.js";
+
+let arr = [4,1,3,6];
+const tree = new Tree(arr);
+
+// Test
+
+console.log(tree);
--- tree.js
+++ tree.js
@@ -1,6 +1,23 @@
+import Node from "./node.js";
+
 export default class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null
+    let sortedArray = array.sort(function (a, b) {
+      return a - b
+    });
+    const middle = parseInt(sortedArray.length / 2);
+    const root = new Node(
+      sortedArray[middle],
+      this.buildTree(sortedArray.slice(0, middle)),
+      this.buildTree(sortedArray.slice(middle + 1))
+    );
+
+    return root
   }
 
 }
